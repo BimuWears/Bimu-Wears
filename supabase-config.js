576b57507484bd87976df1f18ef3dce6775960ee
BimuWears <biha.khan2227@gmail.com>
2026-09-15 19:15:43 +0500

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://lyepdejmhbbmgloqdpfg.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_ksrrV6Ralkeg3SOLrIhcww_t88cuLte';
+export const ADMIN_EMAIL = 'biha.khan2227@gmail.com';
